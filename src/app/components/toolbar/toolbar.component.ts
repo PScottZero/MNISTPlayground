c73a1b0c8ea817a2175ce014d11b3a22bb8db2e2
Paul Scott <8pscott@gmail.com>
2020-09-10 22:06:26 -0400

    mobile fixes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
-  constructor() { }
+  showConfigWindow: boolean;
+  showTrainWindow: boolean;
+  showDrawWindow: boolean;
 
   ngOnInit(): void {
+    if (window.innerWidth < 700) {
+      this.showConfigWindow = false;
+      this.showTrainWindow = false;
+      this.showDrawWindow = false;
+    } else {
+      this.showConfigWindow = true;
+      this.showTrainWindow = true;
+      this.showDrawWindow = true;
+    }
   }
 
+  toggleConfigWindow(): void {
+    this.showConfigWindow = !this.showConfigWindow;
+    if (this.showConfigWindow === true) {
+      this.showTrainWindow = false;
+      this.showDrawWindow = false;
+    }
+  }
+
+  toggleTrainWindow(): void {
+    this.showTrainWindow = !this.showTrainWindow;
+    if (this.showTrainWindow === true) {
+      this.showConfigWindow = false;
+      this.showDrawWindow = false;
+    }
+  }
+
+  toggleDrawWindow(): void {
+    this.showDrawWindow = !this.showDrawWindow;
+    if (this.showDrawWindow === true) {
+      this.showConfigWindow = false;
+      this.showTrainWindow = false;
+    }
+  }
 }
